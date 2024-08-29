24d4f6c17d14d07caacfb4a1c2108c892f26f724
Danilo Silva <danilo.ssilva12@hotmail.com>
2024-08-29 17:43:34 -0300

    feat: adding animation in tab view transition

--- src/components/organinsms/TabView/index.tsx
+++ src/components/organinsms/TabView/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, ReactNode, useEffect } from 'react';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 import { View, YStack } from 'tamagui';
 
 import { TabButton, TabButtonType } from '@/components/atoms';
@@ -10,15 +11,33 @@ interface TabViewProps<T> {
 
 export function TabView<T extends object>({ tabEnum, children }: TabViewProps<T>) {
   const tabTitles = Object.keys(tabEnum).filter((key) => isNaN(Number(key))) as (keyof T)[];
-
   const [selectedTab, setSelectedTab] = useState<number>(0);
+  const opacity = useSharedValue(1);
+  const translateX = useSharedValue(0);
 
   const buttons: TabButtonType[] = tabTitles.map((title) => ({ title: String(title) }));
+
+  useEffect(() => {
+    opacity.value = withTiming(0, { duration: 200 }, () => {
+      opacity.value = withTiming(1, { duration: 200 });
+    });
+
+    translateX.value = withTiming(0 * selectedTab, { duration: 200 });
+  }, [selectedTab]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+      transform: [{ translateX: translateX.value }],
+    };
+  });
 
   return (
     <YStack space={24}>
       <TabButton buttons={buttons} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      <View>{children[selectedTab]}</View>
+      <Animated.View style={animatedStyle}>
+        <View>{children[selectedTab]}</View>
+      </Animated.View>
     </YStack>
   );
 }
